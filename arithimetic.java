4d39e04afc55ecd9a98666844974f1a123117c8d
SHARATHRE <110898279+SHARATHRE@users.noreply.github.com>
2022-08-18 12:18:08 +0530

    Add files via upload

---
+++ arithimetic.java
@@ -0,0 +1,13 @@
+class arithimetic
+  {
+    public static void main(String args[])
+    {
+      int a=30,b=20;
+
+      System.out.println("The addition of "+a+" and "+b+" is = "+(a+b));
+      System.out.println("\nThe subtraction of "+a+" and "+b+" is = "+(a-b));
+      System.out.println("\nThe multiplication of "+a+" and "+b+" is = "+(a*b));
+      System.out.println("\nThe division of "+a+" and "+b+" is = "+(a/b));
+      System.out.println("\nThe modulus of "+a+" and "+b+" is = "+(a%b));
+    }
+  }
