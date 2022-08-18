e0000dbfc80858017d2b293e3267210dbd35f2b3
SHARATHRE <110898279+SHARATHRE@users.noreply.github.com>
2022-08-18 12:47:09 +0530

    Add files via upload

---
+++ areac.java
@@ -0,0 +1,11 @@
+class areac
+  {
+    public static void main(String args[])
+    {
+      int r=10 ;
+        float pi=3.14F;
+      System.out.println("The circle of radius "+r+" has area of =" +(pi*r*r));
+      
+        
+    }
+  }
